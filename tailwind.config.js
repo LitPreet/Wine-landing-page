fc2b868f7c48c8cf050be1f5ba6f4c960f449a4d
Preet bhardwaj <preetb059@gmail.com>
2022-10-09 16:22:43 +0530

    final commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        themepink: '#f2ede3',
+        themebc: '#aea69b',
+        themesp: '#d4c4ae',
+        themebl: '0E0D0C',
+        themefn: '#d6c4a8',
+      },
+      fontFamily:
+      {
+'amer' :['Noto Serif Gujarati', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
